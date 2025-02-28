a317281d08fa2bd377fad4da9f061b8fdc592669
Jimmy Liu <jimmyliu20040@gmail.com>
2025-02-27 18:30:51 -0800

    Nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Home from './components/Home'
+import About from './components/About'
+import Events from './components/Events'
+import Connect from './components/Connect'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // page will either be Home/Events/About/Connect
+  const [page, setPage] = useState('Home')
 
   return (
     <>
+      <header>
+        <button onClick={() => setPage('Home')}>Home</button>
+        <button onClick={() => setPage('Events')}>Events</button>
+        <button onClick={() => setPage('About')}>About Us</button>
+        <button onClick={() => setPage('Connect')}>Connect</button>
+      </header>
       
+      {(page == 'Events') ? <Events /> :
+       (page == 'About') ? <About /> :
+       (page == 'Events') ? <Connect /> :
+       <Home />}
+
+
+      <footer>
+        <img src=""/>
+        <p> partners </p>
+        <p>hiya</p>
+        <p>SpaceX</p>
+        <
+      </footer>
     </>
+
+      
+
   )
 }
 

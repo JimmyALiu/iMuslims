58c544ee13674c238442dc73b3a1425313ae6df2
Jimmy Liu <jimmyliu20040@gmail.com>
2025-04-04 13:00:50 -0700

    Update Header button text and static style.
    -Hover/select not implemented

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,8 +9,8 @@ export default function Header(props) {
             <div className='nav-buttons'>
                 <button className={page == 'Home' ? 'selected-page' : ''} onClick={() => setPage('Home')}>Home</button>
                 <button className={page == 'Events' ? 'selected-page' : ''} onClick={() => setPage('Events')}>Events</button>
-                <button className={page == 'About' ? 'selected-page' : ''} onClick={() => setPage('About')}>About Us</button>
-                <button className={page == 'Connect' ? 'selected-page' : ''} onClick={() => setPage('Connect')}>Let's Connect</button>
+                <button className={page == 'About' ? 'selected-page' : ''} onClick={() => setPage('About')}>About</button>
+                <button className={page == 'Connect' ? 'selected-page' : ''} onClick={() => setPage('Connect')}>Join Us</button>
             </div>
         </header>
     )

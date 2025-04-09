9c6bf3a32e434116eef0ac3f0a031045d68f8526
mattycakes14 <mlau191@uw.edu>
2025-04-08 17:13:02 -0700

    Added grey container for image under how it began header

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
-
+import React from "react";
+import teamImage from "../../public/team-image.png";
 export default function About() {
-    return (
-        <div>About</div>
-    )
+  return (
+    <section>
+      <img src={teamImage} className="teamPicture" />
+      <div className="how">
+        <h1>How It All Began?</h1>
+      </div>
+      <div className="begainContainer"></div>
+      <div className="boardContainer">
+        <h1>Meet Our Board</h1>
+      </div>
+      <div className="beganContainer">
+        <div></div>
+      </div>
+    </section>
+  );
 }

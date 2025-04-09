9bc38c2c4accf9510c7d14157c79450156d9d543
mattycakes14 <mlau191@uw.edu>
2025-04-08 23:03:14 -0700

    Board grid layout implemented

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import teamImage from "../../public/team-image.png";
+import star from "../../public/smoothcorner.png";
+
 export default function About() {
   return (
     <section>
       <img src={teamImage} className="teamPicture" />
       <div className="how">
+        <img className="star" src={star}></img>
         <h1>How It All Began?</h1>
       </div>
       <div className="begainContainer"></div>
       <div className="boardContainer">
         <h1>Meet Our Board</h1>
       </div>
-      <div className="beganContainer">
-        <div></div>
+      <div className="boardGrid">
+        <div className="boardInnerContainer"></div>
+        <div className="boardInnerContainer"></div>
+        <div className="boardInnerContainer"></div>
+        <div className="boardInnerContainer"></div>
+        <div className="boardInnerContainer"></div>
+        <div className="boardInnerContainer"></div>
       </div>
     </section>
   );

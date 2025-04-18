68db7f2bf17b2971905ac8a5d01f5fcfb685eaf2
Jimmy Liu <jimmyliu20040@gmail.com>
2025-04-17 23:54:14 -0700

    Style events banner as demo.

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,7 +1,14 @@
+import "../styles/Events.css"
 import React from 'react'
+import eventsBanner from '../../public/events_banner.png'
 
 export default function Events() {
     return (
-        <div>Events</div>
+        <div className='events-wrapper page-min-width'>
+            <div className='events-banner'>
+                <img src={eventsBanner}></img>
+                <h1 className='page-min-width'>Join Us At Our Next Event?</h1>
+            </div>
+        </div>
     )
 }

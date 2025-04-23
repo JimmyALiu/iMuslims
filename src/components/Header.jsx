65610c200e2a5a381556ca4ab93d4a2d91c74292
Jimmy Liu <jimmyliu20040@gmail.com>
2025-04-22 23:41:52 -0700

    Edit header to load properly.
    Still not fully functional

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,15 @@
 import styles from './Header.module.css';
-import imuslimsLogo from '../../public/circle-logo.png'
-import React from 'react'
+import React from 'react';
 
-export default function Header(props) {
-    const { page, setPage } = props
+export default function Header() {
     return (
-        <header>
-            <img src={imuslimsLogo} className={styles.logo} alt='imuslims logo' />
-            <div className={styles.nav-buttons}>
-                <button className={page == 'Home' ? 'selected-page' : ''} onClick={() => setPage('Home')}>Home</button>
+        <header className={styles.header}>
+            <img src='/circle-logo.png' className={styles.logo} alt='imuslims logo' />
+            <div className={styles.navButtons}>
+                {/* <button className={page == 'Home' ? 'selected-page' : ''} onClick={() => setPage('Home')}>Home</button>
                 <button className={page == 'Events' ? 'selected-page' : ''} onClick={() => setPage('Events')}>Events</button>
                 <button className={page == 'About' ? 'selected-page' : ''} onClick={() => setPage('About')}>About</button>
-                <button className={page == 'Connect' ? 'selected-page' : ''} onClick={() => setPage('Connect')}>Join Us</button>
+                <button className={page == 'Connect' ? 'selected-page' : ''} onClick={() => setPage('Connect')}>Join Us</button> */}
             </div>
         </header>
     )

90e9f9b412ea19e15ae83340eecabd1cf77c401c
Jimmy Liu <jimmyliu20040@gmail.com>
2025-04-23 23:24:38 -0700

    Readd useState variable to retain bold on selected page.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,22 @@
+// inform Next.js that this is a client component, meaning it relies on input from the client
+// this is to retain the bold style on the current button that is selected when you click on a page
+"use client";
+
 import styles from './Header.module.css';
 import React from 'react';
-import Navbar from '../components/Navbar';
+import { useState } from 'react';
+import Navbar from
+
+'../components/Navbar';
 
 export default function Header() {
+    // will either be in the state: 'home', 'events', 'about', or 'connect'
+    const [page, setPage] = useState('home');
+
     return (
         <header className={styles.header}>
             <img src='/circle-logo.png' className={styles.logo} alt='imuslims logo' />
-            <Navbar />
+            <Navbar page={page} setPage={setPage} />
         </header>
     )
 }

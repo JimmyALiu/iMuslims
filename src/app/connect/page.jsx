9ad5b4d58d3521e7bcfc87ddecf927fb271155ff
Jimmy Liu <jimmyliu20040@gmail.com>
2025-05-26 00:13:15 -0700

    Finish banner.

--- src/app/connect/page.jsx
+++ src/app/connect/page.jsx
@@ -1,7 +1,18 @@
+import styles from "./page.module.css"
 import React from 'react'
 
 export default function Connect() {
     return (
-        <div>Connect</div>
+        <section className={styles.connectBody}>
+            <div className={styles.bannerContainer}>
+                <img src='./connect-banner.jpg'></img>
+                <div className={styles.bannerHeader}>
+                    <h1>Get Involved With Us!</h1>
+                </div>
+            </div>
+            <div>
+                <p>body</p>
+            </div>
+        </section>
     )
 }
